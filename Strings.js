53af0876a801fd612d857d770398c81ef88918ab
Gautam <127083244+GitGautamHub@users.noreply.github.com>
2024-01-04 11:01:40 +0000

    strings in javascript

---
+++ Strings.js
@@ -0,0 +1,32 @@
+const name="gautam"
+const repoCount= 50
+
+console.log(name + repoCount +"value")
+
+console.log(`heloo my name is ${name} and my age is 20`)
+
+const gameName= new String("gautammmm")
+
+console.log(gameName[0])
+console.log(gameName.__proto__)
+console.log(gameName.toUpperCase())
+console.log(gameName.length)
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf("t"))
+
+console.log(gameName.substring(0,5))
+
+console.log(gameName.slice(-7,4))
+
+const newString="    gautam        "
+console.log(newString.trim())
+
+const url="https://hitesh.com/hitesh%20chaudhary20"
+console.log(url.replace('%20',"-"))
+
+//other method are:
+/*
+split(separator)
+includes(string)
+*/
+
