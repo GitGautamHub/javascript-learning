5274468aee0c5ef007f3fe97fabd2f83e46573c2
Gautam <127083244+GitGautamHub@users.noreply.github.com>
2024-01-04 10:20:28 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -25,3 +25,35 @@ let stringNumber= String(someNumber)
 console.log(stringNumber)
 
 //33=>"33"
+
+// ***************** Operations ***********************
+
+let value=3
+let negValue= -value
+console.log(negValue)
+console.log(2+2)
+console.log(2*2)
+console.log(2/2)
+console.log(2-2)
+console.log(2^2)
+
+let str1= "hello "
+let str2 ="gautam"
+
+console.log(str1+str2)
+
+
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(true) //true
+console.log(true +2) // 3
+console.log(+true) //1
+
+let num1, num2,num3
+
+num1=num2=num3=2+2
+
+console.table([num1,num2,num3])
