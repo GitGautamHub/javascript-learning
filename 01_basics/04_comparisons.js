33c4d729656eb1a63685157bc3cfa2a56656bc4d
Gautam <127083244+GitGautamHub@users.noreply.github.com>
2024-01-04 10:25:44 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,21 @@
+console.log(2>1)
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>2)
+console.log(null==0)
+console.log(null>=0) // this shows true 
+console.log(null>0)
+
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log(undefined<=0)
+console.log(undefined>0)
+console.log(undefined<0)
+
+//strict check
+//===
+
+console.log("2"===2) // false as strict will check datatype also
+console.log("2"==2)
+
